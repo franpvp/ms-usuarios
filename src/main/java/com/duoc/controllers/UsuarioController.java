fc61cc1c0217cd77d813f4ae52fe459f88ac2e2a
franpvp <franpvp.98@gmail.com>
2025-04-12 21:46:34 -0400

    Se implementa anotacion CrossOrigin

--- src/main/java/com/duoc/controllers/UsuarioController.java
+++ src/main/java/com/duoc/controllers/UsuarioController.java
@@ -12,6 +12,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @Slf4j
+@CrossOrigin(origins = "http://localhost:4200")
 @RestController
 @RequestMapping("/api/v1/usuarios")
 @RequiredArgsConstructor
